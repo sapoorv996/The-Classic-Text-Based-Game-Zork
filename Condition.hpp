718cfaf979f80749088935819de2093fc6c8084c
Geoff <geoff@pal-nat186-69-208.itap.purdue.edu>
2017-10-27 19:58:33 -0400

    Added condition header file

---
+++ Condition.hpp
@@ -0,0 +1,24 @@
+//
+//  Condition.hpp
+//  Zork
+//
+//  Created by Geoff on 10/27/17.
+//  Copyright © 2017 Geoff. All rights reserved.
+//
+
+#ifndef Condition_hpp
+#define Condition_hpp
+
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class Condition {
+public:
+    bool has;
+    string object;
+    string status;
+};
+
+#endif /* Condition_hpp */
